be657e53504688e9e36a2505f09773ff813a6500
Carlos0550 <carlospelinski05@gmail.com>
2024-07-19 23:45:37 -0300

    .

--- src/componentes/Home/ClientInterface/ClientInterface.jsx
+++ src/componentes/Home/ClientInterface/ClientInterface.jsx
@@ -258,7 +258,7 @@ function ClientInterface() {
                                                 <thead>
                                                     <tr>
                                                         <th className='is-background-white is-color-black'>
-                                                            <p className='title has-text-weight-bold is-color-black'>Cliente: {item.nombre_completo || "No hay datos"} {item.apodo ? `(${item.apodo})` : ""}</p>
+                                                            <p className='title has-text-weight-bold is-color-black' style={{textTransform: "capitalize"}}>Cliente: {item.nombre_completo || "No hay datos"} {item.apodo ? `(${item.apodo})` : ""}</p>
                                                         </th>
                                                     </tr>
                                                 </thead>
