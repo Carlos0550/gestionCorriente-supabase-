93f5d17b726b25491b3d2246b8c4c1a3d2ec2e85
Carlos0550 <carlospelinski05@gmail.com>
2024-08-09 10:43:20 -0300

    Implementa validación de entrada en MakeDelivery.jsx

--- src/componentes/Home/Modals/HacerEntrega/MakeDelivery.jsx
+++ src/componentes/Home/Modals/HacerEntrega/MakeDelivery.jsx
@@ -1,21 +1,24 @@
-import React, { useState } from 'react';
-import { Button, Modal, message } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Button, Modal, message, ConfigProvider,DatePicker } from 'antd';
+import { Alert, TextField } from '@mui/material';
 import { useAppContext } from '../../../context';
 import Loader from "../../../Loaders/Loader";
+import esES from 'antd/es/locale/es_ES';
+import 'moment/locale/es';
 
-function MakeDeliver({ closeModal, dataClient, saldo_restante, edit_entrega_data }) {
-  const { 
-    insertDebtTables, 
-    isInserting, 
+function MakeDeliver({ closeModal, dataClient, saldo_restante }) {
+  const {
+    insertDebtTables,
+    isInserting,
     fullDate,
   } = useAppContext();
-  
+
   const client = dataClient[0];
-  
+
   const [values, setValues] = useState({
     idDebt: "",
     monto_entrega: "",
-    fecha_entrega: fullDate,
+    fecha_entrega: "",
     nombre_cliente: client.nombre_completo,
     uuid_cliente: client.uuid,
   });
@@ -25,12 +28,25 @@ function MakeDeliver({ closeModal, dataClient, saldo_restante, edit_entrega_data
   };
 
   const handleInput = (e) => {
-    const { name, value } = e.target;
-    setValues(prevState => ({
-      ...prevState,
-      [name]: value
-    }));
+    if (e && e.target) {
+      const { value, name } = e.target;
+      if (name === "monto_entrega") {
+        const isValid = /^\d*\.?\d*$/.test(value);
+        if (!isValid) return
+      }
+      setValues((prevState) => ({
+        ...prevState,
+        [name]: value
+      }));
+    } else if (e && e.format) {
+      setValues((prevState) => ({
+        ...prevState,
+        fecha_entrega: e.format('DD-MM-YYYY')
+      }));
+    }
   };
+
+
 
   const validateForm = async (ev) => {
     ev.preventDefault();
@@ -39,11 +55,12 @@ function MakeDeliver({ closeModal, dataClient, saldo_restante, edit_entrega_data
     if (!monto_entrega || monto_entrega < 0) {
       message.error("Hay campos vacíos, complételos");
       return;
-    }
-
-    else if (parseFloat(monto_entrega) > saldo_restante) {
+    } else if (parseFloat(monto_entrega) > saldo_restante) {
       message.error(`No puede hacer una entrega mayor a ${saldo_restante}`);
       return;
+    }else if(!values.fecha_entrega){
+        message.error("Seleccione la fecha de entrega",3)
+        return;
     } else {
       await insertDebtTables(values);
       closeModal();
@@ -59,82 +76,61 @@ function MakeDeliver({ closeModal, dataClient, saldo_restante, edit_entrega_data
     >
       <div className='container'>
         <div className="column">
-          <form className='box is-background-white' onSubmit={validateForm} style={{borderRadius: "1rem"}}>
-            <table className="table is-fullwidth is-striped is-hoverable" style={{borderRadius: "1rem", overflow: "hidden"}}>
-              <tbody>
-              <tr>
-                    <td>
-                      <label className='label is-size-4 has-text-weight-bold has-text-black is-background-white'>
-                        Saldo restante:
-                      </label>
-                    </td>
-                    <td>
-                      <p className='title is-size-4 has-text-weight-bold has-text-black m-4' style={{backgroundColor: "#ccc", borderRadius:"15px", padding: "1rem"}}>
-                        ${saldo_restante}
-                      </p>
-                    </td>
-                  </tr>
-                <tr>
-                  <td>
-                    <label className='label is-size-4 has-text-weight-bold has-text-black is-background-white'>
-                      Cantidad a entregar:
-                    </label>
-                  </td>
-                  <td>
-                    <input
-                      type="text"
-                      name="monto_entrega"
-                      value={values.monto_entrega}
-                      onChange={handleInput}
-                      className='input mt-1 is-color-black is-size-5'
-                      style={{ backgroundColor: "#ccc" }}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <label className='label is-color-black is-size-5'>
-                      Fecha de entrega (opcional):
-                    </label>
-                  </td>
-                  <td>
-                    <input
-                      type="text"
-                      name='fecha_entrega'
-                      value={values.fecha_entrega}
-                      onChange={handleInput}
-                      className='input is-color-black is-size-5'
-                      style={{ backgroundColor: "#ccc" }}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td colSpan="2">
-                    <button 
-                      className='button is-info is-size-5 has-text-weight-bold m-3' 
-                      type='submit'
-                      disabled={isInserting}
-                      style={{backgroundColor: isInserting ? "grey": "", cursor: isInserting ? "not-allowed" : ""}}
-                    >
-                      {isInserting ? (
-                        <Loader />
-                      ) : "Entregar"}
-                    </button>
-                    <Button 
-                      type="primary" 
-                      danger 
-                      onClick={handleOk} 
-                      disabled={isInserting}
-                      style={{backgroundColor: isInserting ? "grey": "", cursor: isInserting ? "not-allowed" : ""}}
+          <form className='box is-background-white' onSubmit={validateForm} style={{ borderRadius: "1rem" }}>
+            <div className="flex-container">
+              <Alert severity='info' style={{fontSize: "20px"}}>Saldo restante: ${saldo_restante}</Alert>
+              {/* <TextField
+                id="saldo_restante"
+                label="Saldo restante"
+                value={`$${saldo_restante}`}
+                variant="outlined"
+                fullWidth
+                margin="normal"
+              /> */}
+            </div>
 
-                      className='button is-danger is-size-5 m-3 has-text-weight-bold'
-                    >
-                      Cancelar
-                    </Button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <div className="flex-container">
+              <TextField
+                id="monto_entrega"
+                label="Cantidad a Entregar"
+                name="monto_entrega"
+                value={values.monto_entrega}
+                onChange={handleInput}
+                variant="outlined"
+                fullWidth
+                margin="normal"
+                inputProps={{inputMode: "decimal", pattern: '[0-9]*[.,]?[0-9]*' }}
+              />
+            </div>
+
+            <div className="flex-container" style={{gap: "0"}}>
+            <ConfigProvider locale={esES}>
+                <DatePicker
+                  name='fecha_entrega'
+                  onChange={handleInput}
+                  format={"DD-MM-YYYY"}
+                />
+              </ConfigProvider>
+
+              <button
+                className='button is-info is-size-6 has-text-weight-bold m-3'
+                type='submit'
+                disabled={isInserting}
+                style={{ backgroundColor: isInserting ? "grey" : "", cursor: isInserting ? "not-allowed" : "" }}
+              >
+                {isInserting ? <Loader /> : "Entregar"}
+              </button>
+              <Button
+                type="primary"
+                danger
+                onClick={handleOk}
+                disabled={isInserting}
+                style={{ backgroundColor: isInserting ? "grey" : "", cursor: isInserting ? "not-allowed" : "" }}
+                className='button is-size-6 m-3 has-text-weight-bold'
+              >
+                Cancelar
+              </Button>
+            </div>
           </form>
         </div>
       </div>
