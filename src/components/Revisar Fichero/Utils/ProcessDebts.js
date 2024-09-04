f2e7ee1275d21d585615acc0e06696817f3c2b90
Loscar01 <carlospelinski03@gmail.com>
2024-09-04 12:32:53 -0300

    Procesa la informacion  de las deudas para el componente del fichero del cliente

--- src/components/Revisar Fichero/Utils/ProcessDebts.js
+++ src/components/Revisar Fichero/Utils/ProcessDebts.js
@@ -1,16 +1,21 @@
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
-dayjs.extend(customParseFormat);
+import { totalDelivers } from "./ProcessDelivers";
 
+dayjs.extend(customParseFormat);
 export const formatNames = (name) => {
-    const parts = name.trim().split(" ");
+
+    if (name) {
+      const parts = name.trim().split(" ");
     const capitalizedParts = parts.map((part) => {
       return part.charAt(0).toUpperCase() + part.slice(1).toLowerCase();
     });
     return capitalizedParts.join(" ");
+    }
   };
 export const summationProducts = (products) => {
-    let totalArs = 0;
+    if (products) {
+      let totalArs = 0;
     let totalUsd = 0;
 
     products.forEach(product => {
@@ -22,16 +27,45 @@ export const summationProducts = (products) => {
     });
 
     return (totalArs + totalUsd).toLocaleString("es-ES",{style: "currency", currency: "ARS"});
+    }
   };
 
-export  const groupByDate = (clientDebts) => {
-    const grouped = {};
+export const calculateSubtotal = (debtsData,dataDelivers) => {
+  let total = 0;
+  let totalUsd = 0;
+  let totalArs = 0;
+  const totalDeliveries = totalDelivers(dataDelivers)
+  if (debtsData) {
+    debtsData.forEach(element => {
+      const change = element.change
+      const price = element.price
+      const quantity = element.quantity
+      if (change === "usd") {
+        totalUsd += (parseFloat(price) * parseInt(quantity)) * 1500
+      }else if(change === "ars"){
+        totalArs += parseFloat(price) * parseInt(quantity)
+      }
+    })
+    total = (totalUsd + totalArs) - totalDeliveries
+    return total
+  }
+  // return products.reduce((acc, product)=> {
+  //   const price = parseFloat(product.price)
+  //   const quantity = parseInt(product.quantity)
+  //   return acc += price * quantity
+  // },0)
+}
 
+export  const groupByDate = (clientDebts) => {
+    if (clientDebts) {
+      const grouped = {};
     clientDebts.forEach((element) => {
       const date = dayjs(element.buyDate, "DD-MM-YYYY").format("YYYY-MM-DD");
       if (!grouped[date]) {
         grouped[date] = {
           buyDate: element.buyDate,
+          estado: element.estado,
+          vencimiento: element.duedate,
           products: [],
         };
       }
@@ -42,7 +76,9 @@ export  const groupByDate = (clientDebts) => {
         price: parseFloat(element.price),
         change: element.change,
         quantity: element.quantity,
+        
       });
     });
     return Object.values(grouped);
+    }
   };
