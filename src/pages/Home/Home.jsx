349337dea94a59e1cd09d085f4b9f05047c447de
Loscar01 <carlospelinski03@gmail.com>
2024-09-04 12:33:52 -0300

    refactorizacion temporal de los vencimientos

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,44 @@
-// src/pages/Home.jsx
-import React, { useEffect, useRef, useState } from 'react';
-import { ConfigProvider, Layout, Menu, Table, Input, Typography } from 'antd';
-import { UserOutlined, SearchOutlined, ClockCircleOutlined } from '@ant-design/icons';
-import { useAppContext } from '../../contexto';
-import { ViewClients } from '../../components/VerClientes/ViewClients';
+import React, { useEffect, useRef, useState } from "react";
+import {
+  ConfigProvider,
+  Layout,
+  Menu,
+  Table,
+  Input,
+  Typography,
+  Flex,
+} from "antd";
+import {
+  UserOutlined,
+  SearchOutlined,
+  ClockCircleOutlined,
+} from "@ant-design/icons";
+import { useAppContext } from "../../contexto";
+import { Button } from "antd";
+import { FileSearchOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
 
 const { Header, Content } = Layout;
 const { Search } = Input;
 const { Title } = Typography;
 
 export function Home() {
-  const { fetchClients, clients } = useAppContext();
+  const { fetchClients, clients, fetchHistory } =
+    useAppContext();
   const alreadyFetch = useRef(false);
+
+  const navigate = useNavigate();
+
+  const handleRedirectClient = (link) => {
+    navigate(`/${link}`);
+  };
+
+  const handleRedirectClientHistory = (link) => {
+    navigate(`/${link}`);
+  };
+
+  const [searchText, setSearchText] = useState("");
+  const [activeTab, setActiveTab] = useState("clientes");
 
   useEffect(() => {
     if (!alreadyFetch.current) {
@@ -22,49 +49,154 @@ export function Home() {
     }
   }, [fetchClients]);
 
-  const [searchText, setSearchText] = useState('');
-  const [activeTab, setActiveTab] = useState('clientes');
-
-  
-
   const filteredClientes = clients.filter(
     (cliente) =>
-      cliente.nombre_completo.toLowerCase().includes(searchText.toLowerCase()) ||
-      cliente.dni.includes(searchText)
+      cliente.nombre_completo
+        .toLowerCase()
+        .includes(searchText.toLowerCase()) ||
+      cliente.dni.includes(searchText) ||
+      cliente.apodo.toLowerCase().includes(searchText.toLowerCase())
   );
+
+  // const filteredExpirations = clientExpirations.filter((exp) =>
+  //   exp.nombre_completo.toLowerCase().includes(searchText.toLowerCase())
+  // );
+
+  function ViewClients({ formatNames }) {
+    const columns = [
+      {
+        title: "Nombre",
+        render: (_, record) =>
+          <strong>{formatNames(record.nombre_completo)}</strong> || "N/A",
+        key: "nombre",
+      },
+      {
+        title: "Dni",
+        render: (_, record) => (
+          <strong style={{ color: "black" }}>{record.dni || "N/A"}</strong>
+        ),
+        key: "dni",
+      },
+      {
+        title: "Apodo",
+        render: (_, record) => <p>{formatNames(record.apodo || "N/A")}</p>,
+        key: "apodo",
+      },
+      {
+        title: "Acciones",
+        key: "acciones",
+        render: (_, record) => (
+          <>
+            <Flex vertical wrap gap="small">
+              <Button
+                icon={<FileSearchOutlined />}
+                onClick={() =>
+                  handleRedirectClient(`ver-cliente?clientID=${record.uuid}`)
+                }
+              >
+                Revisar fichero
+              </Button>
+              <Button
+                icon={<FileSearchOutlined />}
+                onClick={() => handleRedirectClientHistory(`ver-historial?clientID=${record.uuid}`)}
+              >
+                Revisar Historial
+              </Button>
+            </Flex>
+          </>
+        ),
+      },
+    ];
+
+    return columns;
+  }
+
+  // function ViewExpirations() {
+  //   const columns = [
+  //     {
+  //       title: "Nombre Cliente",
+  //       key: "nombre_completo",
+  //       render: (_, record) => <strong>{record.nombre_completo}</strong>,
+  //     },
+  //     {
+  //       title: "Fecha de Vencimiento",
+  //       key: "fecha_vencimiento",
+  //       render: (_, record) => <strong>{record.fecha_vencimiento}</strong>,
+  //     },
+  //     {
+  //       title: "Monto",
+  //       key: "monto",
+  //       render: (_, record) => (
+  //         <strong>
+  //           {Number(record.monto).toLocaleString("es-ES", {
+  //             style: "currency",
+  //             currency: "ARS",
+  //           })}
+  //         </strong>
+  //       ),
+  //     },
+  //     {
+  //       title: "Estado",
+  //       key: "estado",
+  //       render: (_, record) => (
+  //         <strong
+  //           style={{ color: record.estado === "activo" ? "green" : "red" }}
+  //         >
+  //           {record.estado === "activo" ? "Al día" : "Vencido"}
+  //         </strong>
+  //       ),
+  //     },
+  //   ];
+
+  //   return columns;
+  // }
+
+  const getColumnas = () => {
+    switch (activeTab) {
+      case "clientes":
+        return ViewClients({ formatNames });
+      // case "vencimientos":
+      //   return ViewExpirations();
+      default:
+        return [];
+    }
+  };
+
+  const getDataSource = () => {
+    if (activeTab === "clientes") return filteredClientes;
+    // if (activeTab === "vencimientos") return filteredExpirations;
+    return [];
+  };
 
   const formatNames = (name) => {
     const parts = name.split(" ");
-    const capitalizedParts = parts.map(part => {
+    const capitalizedParts = parts.map((part) => {
       return part.charAt(0).toUpperCase() + part.slice(1).toLowerCase();
     });
     return capitalizedParts.join(" ");
   };
 
-  // Selecciona las columnas según la pestaña activa
-  const getColumnas = () => {
-    if (activeTab === 'clientes') return ViewClients({ formatNames });
-    // Puedes añadir más condiciones para diferentes tabs y componentes de columnas
-    return [];
-  };
-
   return (
     <ConfigProvider>
       <Layout>
-        <Header style={{ width: '100%', backgroundColor: 'white' }}>
-          <Menu 
-            mode="horizontal" 
+        <Header style={{ width: "100%", backgroundColor: "white" }}>
+          <Menu
+            mode="horizontal"
             selectedKeys={[activeTab]}
             onClick={(e) => setActiveTab(e.key)}
-            style={{ width: '100%' }}
+            style={{ width: "100%" }}
             items={[
-              { key: 'clientes', icon: <UserOutlined />, label: 'Clientes' },
-              { key: 'vencimientos', icon: <ClockCircleOutlined />, label: 'Vencimientos' }
-            ]} 
+              { key: "clientes", icon: <UserOutlined />, label: "Clientes" },
+              // {
+              //   key: "vencimientos",
+              //   icon: <ClockCircleOutlined />,
+              //   label: "Vencimientos",
+              // },
+            ]}
           />
         </Header>
         <Content>
-          <div >
+          <div>
             <Title level={2}>{formatNames(activeTab)}</Title>
             <Search
               placeholder={`Buscar ${formatNames(activeTab)}`}
@@ -73,10 +205,10 @@ export function Home() {
               size="large"
               onChange={(e) => setSearchText(e.target.value)}
             />
-            <Table 
-              columns={getColumnas()} 
-              dataSource={filteredClientes} 
-              scroll={{x:500}}
+            <Table
+              columns={getColumnas()}
+              dataSource={getDataSource()}
+              scroll={{ x: 500 }}
             />
           </div>
         </Content>
