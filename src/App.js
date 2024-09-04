77091f4cd5f3407584422a32931e341f4ae5366c
Loscar01 <carlospelinski03@gmail.com>
2024-09-04 12:31:10 -0300

    Se agrego una ruta para el historial de clientes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes,Route } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import ReviewClientFile from "./components/Revisar Fichero/ReviewClientFile";
+import ViewHistory from "./pages/Home/Columnas/VerHistorial/ViewHistory";
 export default function GestorClientes() {
  
 
@@ -9,6 +10,7 @@ export default function GestorClientes() {
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/ver-cliente" element={<ReviewClientFile/>}/>
+      <Route path="/ver-historial" element={<ViewHistory/>}/>
     </Routes>
   );
 }
