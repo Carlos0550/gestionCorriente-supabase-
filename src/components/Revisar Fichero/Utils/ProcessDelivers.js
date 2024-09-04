690437ccec0a63b18e3baab7687f6a82a04fa7e2
Loscar01 <carlospelinski03@gmail.com>
2024-09-04 12:33:20 -0300

    Procesa informacion de las entregas

--- src/components/Revisar Fichero/Utils/ProcessDelivers.js
+++ src/components/Revisar Fichero/Utils/ProcessDelivers.js
@@ -1,1 +1,6 @@
-export const 
+export const totalDelivers = (deliversData) => {
+    return deliversData.reduce((total, item) => {
+      return total + parseFloat(item.monto_entrega || 0); 
+    }, 0);
+  }
+  
