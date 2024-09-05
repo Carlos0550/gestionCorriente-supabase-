25c96da120bfc06ed1fe1d646c2688e58f7470ef
Loscar01 <carlospelinski03@gmail.com>
2024-09-05 14:38:45 -0300

    se cambio la ruta principal de home al login

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Routes,Route } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import ReviewClientFile from "./components/Revisar Fichero/ReviewClientFile";
 import ViewHistory from "./pages/VerHistorial/ViewHistory";
+import Login from "./pages/Login/Login";
 export default function GestorClientes() {
  
 
   return (
     <Routes>
-      <Route path="/" element={<Home/>}/>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/home" element={<Home/>}/>
       <Route path="/ver-cliente" element={<ReviewClientFile/>}/>
       <Route path="/ver-historial" element={<ViewHistory/>}/>
     </Routes>
